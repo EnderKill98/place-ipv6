fe0e6cfa8883dcf47787fef4cf310b4e09a516d2
EnderKill98 <no@mail.tld>
2024-12-30 17:21:39 +0100

    Update helper scripts with leaks

--- play_video.sh
+++ play_video.sh
@@ -1,5 +1,23 @@
 #export X=100 Y=300 WIDTH=1140 HEIGHT=640
-export X=100 Y=300 WIDTH=860 HEIGHT=480
+#export X=100 Y=300 WIDTH=860 HEIGHT=480
+#export X=0 Y=150 WIDTH=640 HEIGHT=360
 #export X=100 Y=300 WIDTH=1140 HEIGHT=640
-exec ffmpeg -i $1 -vf scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=decrease,pad=$WIDTH:$HEIGHT:-1:-1:color=black,realtime -acodec none -pix_fmt rgb24 -f rawvideo pipe:1 -loglevel quiet \
-  | ./target/release/place-ipv6 -d localhost:1337 -x $X -y $Y -n raw-pipe-stdin $WIDTH $HEIGHT
+#export X=20 Y=150 WIDTH=640 HEIGHT=360
+
+#export X=2320 Y=0 WIDTH=640 HEIGHT=360
+#export X=2320 Y=0 WIDTH=853 HEIGHT=480
+#export X=2320 Y=0 WIDTH=920 HEIGHT=520
+#export X=2320 Y=0 WIDTH=920 HEIGHT=520
+#export X=2320 Y=0 WIDTH=1062 HEIGHT=600
+export X=600 Y=600 WIDTH=852 HEIGHT=480
+#export X=0 Y=0 WIDTH=1920 HEIGHT=1080
+#export X=3110 Y=0 WIDTH=480 HEIGHT=360
+#export X=2950 Y=0 WIDTH=640 HEIGHT=480
+#export X=1920 Y=0 WIDTH=1920 HEIGHT=1080
+
+export ADDR=table.apokalypse.email:1337
+#export ADDR=wall.c3pixelflut.de:1337
+#export ADDR=localhost:1337
+
+exec ffmpeg -i "$1" -vf scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=decrease,pad=$WIDTH:$HEIGHT:-1:-1:color=black,realtime -acodec none -pix_fmt rgb24 -f rawvideo pipe:1 -loglevel quiet \
+  | RUST_BACKTRACE=1 ./target/release/place-ipv6 -c -n -d $ADDR -x $X -y $Y raw-pipe-stdin $WIDTH $HEIGHT -c 1
--- play_yt.sh
+++ play_yt.sh
@@ -1,7 +1,16 @@
 #export X=100 Y=300 WIDTH=1140 HEIGHT=640
-export X=100 Y=300 WIDTH=860 HEIGHT=480
+#export X=100 Y=300 WIDTH=860 HEIGHT=480
 #export X=100 Y=300 WIDTH=1140 HEIGHT=640
 #export X=0 Y=0 WIDTH=1920 HEIGHT=1080
+#export X=3000 Y=500 WIDTH=640 HEIGHT=360
+#export X=0 Y=150 WIDTH=640 HEIGHT=360
+
+export X=20 Y=150 WIDTH=480 HEIGHT=360 # 4:3
+
+export ADDR=table.apokalypse.email:1337
+#export ADDR=wall.c3pixelflut.de:1337
+#export ADDR=localhost:1337
+
 exec yt-dlp $1 -o- \
   | ffmpeg -i pipe:0 -vf scale=$WIDTH:$HEIGHT:force_original_aspect_ratio=decrease,pad=$WIDTH:$HEIGHT:-1:-1:color=black,realtime -acodec none -pix_fmt rgb24 -f rawvideo pipe:1 -loglevel quiet \
-  | ./target/release/place-ipv6 -d localhost:1337 -x $X -y $Y -n raw-pipe-stdin $WIDTH $HEIGHT
+  | ./target/release/place-ipv6 -d $ADDR -x $X -y $Y -n raw-pipe-stdin $WIDTH $HEIGHT -r 100
